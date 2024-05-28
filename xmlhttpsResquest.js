fa58e5a711dd52aaa03e4377fe01e56766fc9b2a
Din Mamma <alexander.gustavsson@skola.taby.se>
2024-05-28 13:14:58 +0200

    asdasdasds

--- xmlhttpsResquest.js
+++ xmlhttpsResquest.js
@@ -1,20 +1,14 @@
-const postBtn = document.getElementById("post-btn")
-const getBtn = document.getElementById("get-btn")
+const searchbar = document.getElementById("searchbar")
 
-const getData = () => {
+searchbar.addEventListener("keydown", (event) => {
+    if (event.key !== "Enter") return
+
+    getData(searchbar.value.toLowerCase().trim())
+})
+
+function getData(query) {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.edamam.com/api/recipes/v2?type=public&q=avocado&app_id=2081b34a&app_key=%20a79725693fda2562221cc273c5bbc7e5%09');
-
-    xhr.onload = () => {
-        const data = JSON.parse(xhr.response);
-    }
+    xhr.open('GET', `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=2081b34a&app_key=%20a79725693fda2562221cc273c5bbc7e5%09`);
 
     xhr.send();
 }
-
-const postData = () => {
-
-}
-
-getBtn.addEventListener("click", getData)
-postBtn.addEventListener("click", postData)

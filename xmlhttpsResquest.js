a268a00db9b4c5bf61177e5fab40b882a7afa79b
Din Mamma <alexander.gustavsson@skola.taby.se>
2024-05-29 23:59:35 +0200

    asdasd

--- xmlhttpsResquest.js
+++ xmlhttpsResquest.js
@@ -30,12 +30,35 @@ function getData(query) {
     if (lastSearch !== query) {
         lastSearch = query
 
+        resultDiv.innerHTML = "";
+
         sendHttpRequest("GET", `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=2081b34a&app_key=%20a79725693fda2562221cc273c5bbc7e5%09`).then((data) => {
+
             for (let i = 0; i < data.hits.length; i++) {
                 
+                let contentSectionDiv = document.createElement('div');
+                contentSectionDiv.className = 'content-section';
+
+                let contentDiv = document.createElement('div');
+                contentDiv.className = 'content';
                 
+                let textDiv = document.createElement('div');
+                textDiv.className = 'text';
+                textDiv.textContent = data.hits[i].recipe.label;
 
+                let imageDiv = document.createElement('div');
+                imageDiv.className = 'image';
+                let image = document.createElement('img');
+                image.setAttribute('src', data.hits[i].recipe.image);
+                image.setAttribute('alt', data.hits[i].recipe.label);
+                image.setAttribute('href', 'https://somewebsite.com');
+                imageDiv.appendChild(image);
 
+                contentDiv.appendChild(textDiv);
+                contentDiv.appendChild(imageDiv);
+                contentSectionDiv.appendChild(contentDiv);
+
+                resultDiv.appendChild(contentSectionDiv);
             }    
         })
     }
